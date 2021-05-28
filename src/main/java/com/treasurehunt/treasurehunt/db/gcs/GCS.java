6aa200055cbd08606e71a256b8c738d185b94711
Chan Rao <lichengrao@gmail.com>
2021-05-28 18:23:22 -0400

    Add remove picture method for GCS

--- src/main/java/com/treasurehunt/treasurehunt/db/gcs/GCS.java
+++ src/main/java/com/treasurehunt/treasurehunt/db/gcs/GCS.java
@@ -12,21 +12,35 @@ public class GCS {
     private static final String bucketName = "treasure-hunt-listing-pictures";
 
     // Upload picture to listing-pictures bucket in GCS
-    public static String uploadPicture(Storage storage, String fileName, InputStream fileInputStream) throws GCSException {
+    public static String uploadPicture(Storage storage, String pictureId, InputStream fileInputStream) throws GCSException {
         try {
             // Create blobId and blobInfo
-            BlobId blobId = BlobId.of(bucketName, fileName);
+            BlobId blobId = BlobId.of(bucketName, pictureId);
             BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
 
             // Upload blob into GCS
             Blob blob = storage.createFrom(blobInfo, fileInputStream);
 
             // Return the media link of the uploaded blob
-            System.out.println("File uploaded to " + bucketName + " as " + fileName);
+            System.out.println("Picture uploaded to " + bucketName + " as " + pictureId);
             return blob.getMediaLink();
         } catch (Exception e) {
             e.printStackTrace();
-            throw new GCSException(String.format("Failed to upload %s to %s", fileName, bucketName));
+            throw new GCSException(String.format("Failed to upload %s to %s", pictureId, bucketName));
+        }
+    }
+
+    // Remove uploaded picture from listing-pictures bucket in GCS
+    public static void deletePicture(Storage storage, String pictureId) throws GCSException {
+        try {
+
+            // Delete picture from picture bucket
+            storage.delete(bucketName, pictureId);
+
+            System.out.println("Picture " + pictureId + " was deleted from " + bucketName);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new GCSException(String.format("Failed to delete %s from %s", pictureId, bucketName));
         }
     }
 }
