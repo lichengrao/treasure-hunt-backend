576bbaaccc48e5160c0eb31df9eff3c2e60da109
Chan Rao <lichengrao@gmail.com>
2021-05-30 23:54:53 -0400

    fix: Remove foreign key constraint saved_records
    Removed saved_records.listing_id reference to listings db

--- src/main/java/com/treasurehunt/treasurehunt/db/mysql/MySQLTableCreator.java
+++ src/main/java/com/treasurehunt/treasurehunt/db/mysql/MySQLTableCreator.java
@@ -73,8 +73,7 @@ public class MySQLTableCreator {
                         + "user_id VARCHAR(255) NOT NULL,"
                         + "listing_id VARCHAR(255) NOT NULL,"
                         + "PRIMARY KEY (user_id, listing_id),"
-                        + "FOREIGN KEY (user_id) REFERENCES users(user_id),"
-                        + "FOREIGN KEY (listing_id) REFERENCES listings(listing_id)"
+                        + "FOREIGN KEY (user_id) REFERENCES users(user_id) "
                         + ")";
                 statement.executeUpdate(sql);
 
